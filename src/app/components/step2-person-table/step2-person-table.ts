449d7c6c75a98a3a53d82613a22fc64636722220
nikosasimakopouloss <nikosasimakopouloss@gmail.com>
2025-12-26 03:52:51 +0200

    app-step2-person-table

---
+++ src/app/components/step2-person-table/step2-person-table.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step2-person-table',
+  imports: [],
+  templateUrl: './step2-person-table.html',
+  styleUrl: './step2-person-table.css',
+})
+export class Step2PersonTable {
+
+  user = {
+
+    firstname: "Bob",
+    lastname: "Dylan",
+    email: "bob.dylan@aueb.gr"
+  }
+
+}
